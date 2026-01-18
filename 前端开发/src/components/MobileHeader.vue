2792af1191e7cebc9c465a4dd7cfe05059f597bd
pandax <8089955@gmail.com>
2026-01-18 14:40:31 +0800

    Fix avatar fallback icon rendering in MobileHeader
    Replaces v-else with v-if="!avatarUrl" for the user icon in MobileHeader.vue to ensure the fallback icon only renders when no avatar URL is present. Also updates the Android build workflow to use an environment variable for the signing key check.

--- 前端开发/src/components/MobileHeader.vue
+++ 前端开发/src/components/MobileHeader.vue
@@ -56,7 +56,7 @@ const pageTitle = computed(() => {
       </button>
       <div v-if="userStore.isLoggedIn" class="w-8 h-8 rounded-full overflow-hidden border border-primary/20 bg-gray-100">
         <img v-if="avatarUrl" :src="avatarUrl" alt="User" class="w-full h-full object-cover" />
-        <i v-else data-lucide="user" class="w-5 h-5 m-1.5 text-on-surface-variant"></i>
+        <i v-if="!avatarUrl" data-lucide="user" class="w-5 h-5 m-1.5 text-on-surface-variant"></i>
       </div>
     </div>
   </header>
